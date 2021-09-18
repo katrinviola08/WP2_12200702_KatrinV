631b22463f07ea79e0d8abf83400311f1f768d2b
katrinviola08 <kviolajessica@gmail.com>
2021-09-18 17:23:25 +0700

    penambahan isi contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Katrin Viola Jessica
+        Saya tinggal di daerah Kayu Putih
+        Hobby yang saya sukai adalah 
+        Mendengarkan musik";
+    }
+}
