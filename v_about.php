520c45623f38a5d0ae0bbbe8cd4e5b38e3632c6e
katrinviola08 <kviolajessica@gmail.com>
2021-10-11 13:40:14 +0700

    Upload Tugas

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Katrin Viola</li> 
+        <li>Nama Belakang : Jessica</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan Bangunan Barat No.21</li> 
+    </ul> 
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none"> 
+        <li>Jakarta</li> 
+    </ul> 
+    
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Berenang</li> 
+        <li>Volley</li> 
+    </ul> 
+</section>
